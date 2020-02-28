ed080990527656e0effd49ef641231d0c710a56e
Jeremy Gill <jeremygill87@gmail.com>
2020-02-28 13:59:14 -0500

    Updates

---
+++ precache-manifest.0e91f64796cdf4abf4eafa5052f7117e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392b630f4cb3cc5bb00c773aebfaf640",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "bcb1e0eb04444757ba7c",
+    "url": "/ClickyGame/static/css/main.740c0678.chunk.css"
+  },
+  {
+    "revision": "ecff1ac4b3f00c354d33",
+    "url": "/ClickyGame/static/js/2.74512566.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ClickyGame/static/js/2.74512566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb1e0eb04444757ba7c",
+    "url": "/ClickyGame/static/js/main.ccd756ee.chunk.js"
+  },
+  {
+    "revision": "88489d381a86dae2d822",
+    "url": "/ClickyGame/static/js/runtime-main.1386749d.js"
+  }
+]);
